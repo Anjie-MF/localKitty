51626e2a9c84f5379bd9beb3e965741b8758cea7
anjie_f <amfarmer2022@gmail.com>
2024-03-08 12:42:41 -0600

    Merged topic into main

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 var button = document.querySelector(".show-cat");
-    button.addEventListener("click", function(){
-        cat.classList.add("show")
-    });
 var cat = document.querySelector(".cat");
+
+button.addEventListener("click", function () {
+    if (cat.classList.contains("show")) {
+        //console.log("Yes!");
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+    } else {
+        //console.log("Nope!");
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!!";
+    }
+});
+
 
